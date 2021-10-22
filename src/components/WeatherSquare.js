4b2db526d4a1631a855c8f4b971b2ab7de02aa79
has992 <89402453+has992@users.noreply.github.com>
2021-10-22 21:29:38 +0100

    initial commit

---
+++ src/components/WeatherSquare.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const WeatherSquare = ({getWeather}) => {
+    return(
+        <div className="wSquare">
+            <h1>Gangs of London</h1>
+             <div className="btn">
+                <button type="button" onClick={() => getWeather()} className="btn btn-warning">Get Weather</button>
+             </div>
+        </div>
+
+    )
+
+}
